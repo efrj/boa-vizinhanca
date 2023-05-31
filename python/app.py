7b7d46509f9e48a36a1106438ad5ff056f23c344
Edivaldo Reis <efrj@hotmail.com>
2023-05-31 00:32:19 -0300

    Updated API's

--- python/app.py
+++ python/app.py
@@ -1,30 +1,43 @@
 import random
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-# Lista de frases
+# Frases do Professor Girafales
 frases = [
-    'Se não for incômodo',
-    'Vim lhe trazer este humilde presente',
-    'Depois da senhora',
+    'Se não for incômodo.',
+    'Vim lhe trazer este humilde presente.',
+    'Depois da senhora.',
     'Dizia eu que a aritmética...',
-    'Silêêêêêncioooo...',
+    'Silêêêêêncioooo!!!',
     'Tá! Tá! Tá! Tá! Tá!',
-    'Só me enganei uma vez, quando pensei estar enganado!',
-    'Quero ver outra vez seus olhinhos de noite serena.'
+    'Eu jamais me engano. Só me enganei uma vez: quando acreditei estar enganado!',
+    'Quero ver outra vez seus olhinhos de noite serena.',
+    'Dooooooooooona Florinda.',
+    'Sou Linguiça de sobrenome mestre, digo, sou Professor e meu nome é Girafales.',
+    'Por que causa, motivo, razão ou circunstância...',
+    'Que foi que você disse?',
+    'Gooooooooooooooooooooool de Peléeeeeeeeeeeeeeeeeee ! Ele rebateu de cabeça, quando não havia esperança.',
+    'A criança que amanhã será homem, a semente que amanhã será fruto, ao casulo que amanhã será mariposa.',
+    'Saiba que tudo que se vende pelas ruas faz mal. Por exemplo : churrasquinho, os refrescos, sanduíches daqueles que vem com maionese, com tomate, cachorro quente, empadas, os pastéis, coxinhas, risoles, etc, etc...',
+    'Bem, esta representação teatral foi montada e dirigida pelo Seu Madruga, mas, por favor, não caçoem dele. Talvez a vocês o trabalho dele pareça tolo, inútil, comum, vulgar. Sim, concordo. Mas é que devem levar em conta que se trata de um indivíduo sem nenhum preparo. De um pobre diabo que nem sequer concluiu o primário. De um pobre infeliz que mal aprendeu a ler e a escrever. De um reles... deixe me continuar, Seu Madruga, de um João Ninguém...',
+    'Acapulco me espera, Acapulco me espera...',
+    'Eu já tive alunos bons, regulares, ruins, péssimos e o Quico. Mas não se preocupe, é provável que haja piores.',
+    'Dona Florinda não está acostumada a ser paquerada pelos velhos gordos.',
+    'Eu sou inimigo fidagal da violência, mas se ver o Chaves com a luva de boxe, eu, Professor Girafales, vou quebrar tudo que se chama cara.',
+    'Quico, quando é que vai entrar no compasso?',
+    'Como ousa ofender essa digníssima dama?'
 ]
 
-# Classe handler para tratar as requisições HTTP
 class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
+        self.send_header('Content-type', 'text/html;charset=UTF-8')
+        self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
 
         # Selecionar uma frase aleatória
         frase = random.choice(frases)
         self.wfile.write(frase.encode())
 
-# Função para rodar o servidor HTTP
 def run_server():
     host = '0.0.0.0'
     port = 8000
@@ -33,6 +46,5 @@ def run_server():
     print(f'Servidor rodando em http://{host}:{port}/')
     httpd.serve_forever()
 
-# Iniciar o servidor
 if __name__ == '__main__':
     run_server()
