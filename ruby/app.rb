b80a44a92e3e0df3d524a371d77020e0719dea69
Edivaldo Reis <efrj@hotmail.com>
2023-10-14 20:24:00 -0300

    Changed the Ruby application to read the sentences of the character Dona Florinda contained in the json file.

--- ruby/app.rb
+++ ruby/app.rb
@@ -1,23 +1,13 @@
 require 'webrick'
+require 'json'
 
-PHRASES = [
-  'Professor Girafales! Que milagre o senhor por aqui',
-  'Não gostaria de entrar para tomar uma xícara de café?',
-  'Vamos, Tesouro, não se misture com essa gentalha!',
-  'E da próxima vez vá fazer isso para sua vó!',
-  'Tinha que ser o Chaves mesmo.',
-  'Tesooooouro.',
-  'Fredericooo.',
-  'Porque se a gentalha vai para Acapulco, eu também vou!',
-  'Tesouro, por favor! Lembre-se que estamos num hotel de categoria. Não na casa do Seu Madruga...',
-  'O que a Maitê Proença tem que eu não tenho?!',
-  'Todo mundo chora com cebola.'
-]
+phrases = JSON.parse(File.read(File.expand_path('phrases/phrases.json', __dir__)))
 
 server = WEBrick::HTTPServer.new(Port: 8000)
 
 server.mount_proc '/' do |request, response|
-  random_phrase = PHRASES.sample
+  donaFlorindaPhrases = phrases['dona_florinda']
+  random_phrase = donaFlorindaPhrases.sample
   response['Content-Type'] = 'text/html;charset=UTF-8'
   response['Access-Control-Allow-Origin'] = '*'
   response['Access-Control-Allow-Methods'] = 'GET'
