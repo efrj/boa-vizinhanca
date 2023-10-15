3063b32cacb43f7fa669fb8082aef99cd0fd37ca
Edivaldo Reis <efrj@hotmail.com>
2023-10-15 14:44:02 -0300

    Changed the Shell Script application to read the sentences of the character Jaiminho contained in the json file.

--- shell-script/Jaiminho.sh
+++ shell-script/Jaiminho.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+json_file='phrases/phrases.json'
+
 while true; do
-  phrase=$(shuf -n 1 -e "Eu sou de Tangamandápio" "Sou Jaiminho, o carteiro" "Eu quero evitar a fadiga" "Tangamandápio é minha terra natal. Um lindo povoado com crepúsculos avermelhados..." "Cadê a minha torta?")
+  phrase=$(jq -r '.jaiminho | .[]' "$json_file" | shuf -n 1)
   echo -e "HTTP/1.1 200 OK\nContent-Type: text/html;charset=UTF-8\nAccess-Control-Allow-Origin: *\n\n$phrase" | nc -l -p 80 -q 1
 done
