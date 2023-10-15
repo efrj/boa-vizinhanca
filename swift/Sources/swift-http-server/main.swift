ba7931952a78ba001a3e1aecf21be6e0226456f5
Edivaldo Reis <efrj@hotmail.com>
2023-10-15 14:44:35 -0300

    Changed the Sift application to read the sentences of the character Dona Clotilde contained in the json file.

--- swift/Sources/swift-http-server/main.swift
+++ swift/Sources/swift-http-server/main.swift
@@ -1,22 +1,20 @@
 import Kitura
+import Foundation
 
-let phrases = [
-    "Quem é bruxa? Eu não sou nenhuma bruxa!",
-    "Como? Chamou quem de bruxa?",
-    "Boneco, simpático!",
-    "Madruguinha lindo!",
-    "O quê? É melhor não dizer nada!",
-    "Ai, Seu Madruga, o senhor me encabula!",
-    "Drudru, Gatão, Galã!",
-    "Tinha que ser o Chaves!",
-    "Eu vou para Acapulco tomar banho de mar...",
-    "Satanás! Cadê você Satanás? Satanááááááás!"
-]
+let currentDirectory = FileManager.default.currentDirectoryPath
+let jsonPath = currentDirectory + "/phrases/phrases.json"
+
+guard let jsonData = FileManager.default.contents(atPath: jsonPath) else {
+    fatalError("Não foi possível ler o arquivo JSON")
+}
+
+let json = try! JSONSerialization.jsonObject(with: jsonData, options: []) as! [String: [String]]
+let donaClotildePhrases = json["dona_clotilde"] ?? []
 
 let router = Router()
 
 router.get("/") { _, response, _ in
-    let randomPhrase = phrases.randomElement() ?? ""
+    let randomPhrase = donaClotildePhrases.randomElement() ?? ""
     response.headers.setType("text/html", charset: "UTF-8")
     response.headers["Access-Control-Allow-Origin"] = "*"
     response.send(randomPhrase)
