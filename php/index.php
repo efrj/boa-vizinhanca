f0393e5e4e839311e57dec250172ac17b779e018
Edivaldo Reis <efrj@hotmail.com>
2024-03-10 07:56:39 -0300

    Replacing array_rand with random_int to obtain random index of phrases in PHP application

--- php/index.php
+++ php/index.php
@@ -6,7 +6,7 @@ $phrases = json_decode(file_get_contents('phrases/phrases.json'), true);
 
 $chavesPhrases = $phrases['chaves'];
 
-$indice = array_rand($chavesPhrases);
+$indice = random_int(0, count($chavesPhrases) - 1);
 $randomphrase = $chavesPhrases[$indice];
 
 echo $randomphrase;
