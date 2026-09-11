8f8eb2037ea6928f6e33ee43a2990fd388033404
Edivaldo Reis <efrj@hotmail.com>
2026-09-11 18:00:01 -0300

    feat(frontend): apply unique non-repeating theme colors to all character cards

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -12,21 +12,21 @@ document.addEventListener('alpine:init', () => {
 
         characters: [
             { id: 1, name: 'Chaves', port: 8001, lang: 'PHP', avatar: '/img/chaves.png', langLogo: '/img/php.png', phrase: 'Carregando...', loading: true, status: 'loading', style: 'bg-primary' },
-            { id: 2, name: 'Dona Florinda', port: 8004, lang: 'Python', avatar: '/img/dona-florinda.png', langLogo: '/img/python.png', phrase: 'Carregando...', loading: true, status: 'loading', style: 'bg-danger' },
-            { id: 3, name: 'Seu Madruga', port: 8005, lang: 'Lua', avatar: '/img/seu-madruga.png', langLogo: '/img/lua.png', phrase: 'Carregando...', loading: true, status: 'loading', style: 'bg-info' },
-            { id: 4, name: 'Prof. Girafales', port: 8002, lang: 'Ruby', avatar: '/img/professor-girafales.png', langLogo: '/img/ruby.png', phrase: 'Carregando...', loading: true, status: 'loading', style: 'bg-warning' },
-            { id: 5, name: 'Quico', port: 8003, lang: 'Node.js', avatar: '/img/quico.png', langLogo: '/img/nodejs.png', phrase: 'Carregando...', loading: true, status: 'loading', style: 'bg-danger' },
-            { id: 6, name: 'Chiquinha', port: 8006, lang: 'Perl', avatar: '/img/chiquinha.png', langLogo: '/img/perl.png', phrase: 'Carregando...', loading: true, status: 'loading', style: 'bg-success' },
-            { id: 7, name: 'Nhonho', port: 8007, lang: 'Go', avatar: '/img/nhonho.png', langLogo: '/img/go.png', phrase: 'Carregando...', loading: true, status: 'loading', style: 'bg-warning' },
-            { id: 8, name: 'Seu Barriga', port: 8008, lang: 'Java', avatar: '/img/seu-barriga.png', langLogo: '/img/java.png', phrase: 'Carregando...', loading: true, status: 'loading', style: 'bg-dark' },
-            { id: 9, name: 'Dona Clotilde', port: 8009, lang: 'Swift', avatar: '/img/dona-clotilde.png', langLogo: '/img/swift.png', phrase: 'Carregando...', loading: true, status: 'loading', style: 'bg-info' },
-            { id: 10, name: 'Carteiro Jaiminho', port: 8010, lang: 'Shell Script', avatar: '/img/jaiminho.png', langLogo: '/img/sh.png', phrase: 'Carregando...', loading: true, status: 'loading', style: 'bg-primary' },
-            { id: 11, name: 'Popis', port: 8011, lang: 'Crystal', avatar: '/img/popis.png', langLogo: '/img/crystal.png', phrase: 'Carregando...', loading: true, status: 'loading', style: 'bg-danger' },
-            { id: 12, name: 'Godinez', port: 8012, lang: 'TypeScript', avatar: '/img/godinez.png', langLogo: '/img/typescript.png', phrase: 'Carregando...', loading: true, status: 'loading', style: 'bg-success' },
-            { id: 13, name: 'Paty', port: 8013, lang: 'CoffeeScript', avatar: '/img/paty.png', langLogo: '/img/coffeescript.png', phrase: 'Carregando...', loading: true, status: 'loading', style: 'bg-info' },
-            { id: 16, name: 'Dona Neves', port: 8016, lang: 'Scala', avatar: '/img/dona-neves.png', langLogo: '/img/scala.png', phrase: 'Carregando...', loading: true, status: 'loading', style: 'bg-danger' },
-            { id: 14, name: 'Dr. Chapatin', port: 8014, lang: 'C++', avatar: '/img/dr-chapatin.png', langLogo: '/img/c++.png', phrase: 'Carregando...', loading: true, status: 'loading', style: 'bg-danger' },
-            { id: 15, name: 'Chapolin Colorado', port: 8015, lang: 'Dart', avatar: '/img/chapolin-colorado.png', langLogo: '/img/dart.png', phrase: 'Carregando...', loading: true, status: 'loading', style: 'bg-warning' }
+            { id: 2, name: 'Dona Florinda', port: 8004, lang: 'Python', avatar: '/img/dona-florinda.png', langLogo: '/img/python.png', phrase: 'Carregando...', loading: true, status: 'loading', style: 'card-theme-dona-florinda' },
+            { id: 3, name: 'Seu Madruga', port: 8005, lang: 'Lua', avatar: '/img/seu-madruga.png', langLogo: '/img/lua.png', phrase: 'Carregando...', loading: true, status: 'loading', style: 'card-theme-seu-madruga' },
+            { id: 4, name: 'Prof. Girafales', port: 8002, lang: 'Ruby', avatar: '/img/professor-girafales.png', langLogo: '/img/ruby.png', phrase: 'Carregando...', loading: true, status: 'loading', style: 'card-theme-professor-girafales' },
+            { id: 5, name: 'Quico', port: 8003, lang: 'Node.js', avatar: '/img/quico.png', langLogo: '/img/nodejs.png', phrase: 'Carregando...', loading: true, status: 'loading', style: 'card-theme-quico' },
+            { id: 6, name: 'Chiquinha', port: 8006, lang: 'Perl', avatar: '/img/chiquinha.png', langLogo: '/img/perl.png', phrase: 'Carregando...', loading: true, status: 'loading', style: 'card-theme-chiquinha' },
+            { id: 7, name: 'Nhonho', port: 8007, lang: 'Go', avatar: '/img/nhonho.png', langLogo: '/img/go.png', phrase: 'Carregando...', loading: true, status: 'loading', style: 'card-theme-nhonho' },
+            { id: 8, name: 'Seu Barriga', port: 8008, lang: 'Java', avatar: '/img/seu-barriga.png', langLogo: '/img/java.png', phrase: 'Carregando...', loading: true, status: 'loading', style: 'card-theme-seu-barriga' },
+            { id: 9, name: 'Dona Clotilde', port: 8009, lang: 'Swift', avatar: '/img/dona-clotilde.png', langLogo: '/img/swift.png', phrase: 'Carregando...', loading: true, status: 'loading', style: 'card-theme-dona-clotilde' },
+            { id: 10, name: 'Carteiro Jaiminho', port: 8010, lang: 'Shell Script', avatar: '/img/jaiminho.png', langLogo: '/img/sh.png', phrase: 'Carregando...', loading: true, status: 'loading', style: 'card-theme-jaiminho' },
+            { id: 11, name: 'Popis', port: 8011, lang: 'Crystal', avatar: '/img/popis.png', langLogo: '/img/crystal.png', phrase: 'Carregando...', loading: true, status: 'loading', style: 'card-theme-popis' },
+            { id: 12, name: 'Godinez', port: 8012, lang: 'TypeScript', avatar: '/img/godinez.png', langLogo: '/img/typescript.png', phrase: 'Carregando...', loading: true, status: 'loading', style: 'card-theme-godinez' },
+            { id: 13, name: 'Paty', port: 8013, lang: 'CoffeeScript', avatar: '/img/paty.png', langLogo: '/img/coffeescript.png', phrase: 'Carregando...', loading: true, status: 'loading', style: 'card-theme-paty' },
+            { id: 16, name: 'Dona Neves', port: 8016, lang: 'Scala', avatar: '/img/dona-neves.png', langLogo: '/img/scala.png', phrase: 'Carregando...', loading: true, status: 'loading', style: 'card-theme-dona-neves' },
+            { id: 14, name: 'Dr. Chapatin', port: 8014, lang: 'C++', avatar: '/img/dr-chapatin.png', langLogo: '/img/c++.png', phrase: 'Carregando...', loading: true, status: 'loading', style: 'card-theme-dr-chapatin' },
+            { id: 15, name: 'Chapolin Colorado', port: 8015, lang: 'Dart', avatar: '/img/chapolin-colorado.png', langLogo: '/img/dart.png', phrase: 'Carregando...', loading: true, status: 'loading', style: 'card-theme-chapolin-colorado' }
         ],
 
         async init() {
